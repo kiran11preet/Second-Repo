1b9d0bc6eda6773ccff6ebf4f99ecf26f9ea913f
kiran11preet <kiranpreet111193@gmail.com>
2023-02-11 13:18:30 -0500

    first commit

---
+++ src/ForClass.java
@@ -0,0 +1,11 @@
+public class ForClass {
+    public static void main(String []args) {
+        for(int a = 0 ; a <= 1000 ; a++) {
+            if (a % 2 == 0) {
+                System.out.println(a);
+            }
+            }
+
+        }
+
+    }
